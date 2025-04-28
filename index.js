10cf2d82467e7301af793f046a7d78fdacb69339
PrachiKhandelwal <prachik27200001@gmail.com>
2025-04-29 01:09:07 +0530

    replaced manual action dispatching with bindActionCreators

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const redux = require("redux");
 
 const createStore = redux.createStore;
 
+const bindActionCreators = redux.bindActionCreators;
+
 const CAKE_ORDERED = "CAKE_ORDERED";
 
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
@@ -48,9 +50,15 @@ const unsubscribe = store.subscribe(() =>
     console.log("updated state: ", store.getState())
 );
 
-store.dispatch(orderCake(3));
-store.dispatch(orderCake(1));
-store.dispatch(restockCake(5));
-unsubscribe();
-store.dispatch(orderCake(1));
-console.log(store.getState());
+// store.dispatch(orderCake(3));
+// store.dispatch(orderCake(1));
+// store.dispatch(restockCake(5));
+// unsubscribe();
+// store.dispatch(orderCake(1));
+// console.log(store.getState());
+
+//bindActionCreators are helper functions that allow us to call action creators directly without manually dispatching them
+
+const actions = bindActionCreators({orderCake,restockCake},store.dispatch);
+actions.orderCake(1);
+actions.restockCake(5);
