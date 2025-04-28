9f04b3ea01b4aaa409e9d89c10824650fa7eab99
PrachiKhandelwal <prachik27200001@gmail.com>
2025-04-29 02:20:39 +0530

    handling updation of nested state

---
+++ nested-state.js
@@ -0,0 +1,46 @@
+const redux = require("redux");
+
+const createStore = redux.createStore;
+
+const bindActionCreators = redux.bindActionCreators;
+
+const STREET_UPDATED = "STREET_UPDATED";
+
+const updateStreet = (newStr) => {
+    return {
+        type: STREET_UPDATED,
+        payload: newStr,
+    };
+};
+
+const initialState = {
+    name: "Prachi",
+    address: {
+        street: "street 12",
+        city: "bangalore",
+        state: "karnataka",
+    },
+};
+
+const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case STREET_UPDATED:
+            return {
+                ...state,
+                address: { ...state.address, street: action.payload },
+            };
+        default:
+            return state;
+    }
+};
+
+const store = createStore(userReducer);
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
+
+const actions=bindActionCreators({updateStreet},store.dispatch);
+
+actions.updateStreet("street 9");
+unsubscribe();
